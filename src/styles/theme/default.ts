11a6504186d9bb81d644cc0fe791458f6b06e2b1
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2022-12-26 08:10:58 -0300

    feat: add default theme

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,28 @@
+export const defaultTheme = {
+  colors: {
+    normal: {
+      'base-background': '#171930',
+      'base-task': '#1E203B',
+      'base-input': '#101227',
+      'base-placeholder': '#71 717A',
+      'base-title': '#FFFFFF',
+      'base-text': '#A3A3A3',
+      'base-progress': '#13B57A',
+      'base-error': '#F84747',
+    },
+    gradient: {
+      'base-card': '180deg, #654EA3 0%, #EAAFC8 100%',
+      'add-button': '200deg, #654EA3 0%, #654EA300 740%',
+      'priority-button': '183deg, 654EA386 0%, #EAAFC8 600%',
+      'brand-blue': '135deg, #266ED7 60%, #4D8AEB 150%',
+      'brand-orange': '180deg, #ffc92c 0%, #fd9371 100%',
+    },
+  },
+  textSizes: {
+    'text-s': '0.75rem',
+    'text-m': '0.875rem',
+    'heading-s': '0.875rem',
+    'heading-m': '1.25rem',
+    'heading-l': '1.5rem',
+  },
+};
