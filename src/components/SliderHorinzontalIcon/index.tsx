44047e5f6ada7b9958b5f639872dc41f7ed39ee5
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2022-12-28 12:46:37 -0300

    feat: add slider horizontal icon component

---
+++ src/components/SliderHorinzontalIcon/index.tsx
@@ -0,0 +1,110 @@
+export function SliderHorizontalIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path
+        stroke="url(#paint0_linear_95_13)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.875 16.125H3.75"
+      ></path>
+      <path
+        stroke="url(#paint1_linear_95_13)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M20.25 16.125h-2.625"
+      ></path>
+      <path
+        stroke="url(#paint2_linear_95_13)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 18a1.875 1.875 0 100-3.75 1.875 1.875 0 000 3.75z"
+      ></path>
+      <path
+        stroke="url(#paint3_linear_95_13)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M7.875 7.875H3.75"
+      ></path>
+      <path
+        stroke="url(#paint4_linear_95_13)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M20.25 7.875h-8.625"
+      ></path>
+      <path
+        stroke="url(#paint5_linear_95_13)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9.75 9.75a1.875 1.875 0 100-3.75 1.875 1.875 0 000 3.75z"
+      ></path>
+      <defs>
+        <linearGradient
+          id="paint0_linear_95_13"
+          x1="-0.585"
+          x2="-0.361"
+          y1="16.762"
+          y2="18.728"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#266ED7"></stop>
+          <stop offset="1" stopColor="#4D8AEB"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_95_13"
+          x1="16.501"
+          x2="17.234"
+          y1="16.762"
+          y2="18.432"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#266ED7"></stop>
+          <stop offset="1" stopColor="#4D8AEB"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_95_13"
+          x1="12.27"
+          x2="15.967"
+          y1="16.638"
+          y2="19.849"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#266ED7"></stop>
+          <stop offset="1" stopColor="#4D8AEB"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_95_13"
+          x1="1.984"
+          x2="2.5"
+          y1="8.512"
+          y2="10.359"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#266ED7"></stop>
+          <stop offset="1" stopColor="#4D8AEB"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_95_13"
+          x1="7.932"
+          x2="8.194"
+          y1="8.512"
+          y2="10.468"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#266ED7"></stop>
+          <stop offset="1" stopColor="#4D8AEB"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint5_linear_95_13"
+          x1="6.269"
+          x2="9.967"
+          y1="8.388"
+          y2="11.599"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#266ED7"></stop>
+          <stop offset="1" stopColor="#4D8AEB"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
