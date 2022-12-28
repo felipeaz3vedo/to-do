8e3e95e0060976847dd039bdd6fc29260e7ac340
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2022-12-28 11:02:14 -0300

    feat: add magnifying glass icon component

---
+++ src/components/IconsComponents/MagnifyingGlassIcon/index.tsx
@@ -0,0 +1,13 @@
+export function MagnifyingGlassIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20">
+      <path
+        stroke="#71717A"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeOpacity="0.7"
+        d="M9.063 15.625a6.562 6.562 0 100-13.125 6.562 6.562 0 000 13.125zM13.703 13.703L17.5 17.5"
+      ></path>
+    </svg>
+  );
+}
